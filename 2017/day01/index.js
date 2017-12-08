be78b55d2bb5a005138e9e6937d49746a3869fe7
Kyle Farnung <kfarnung@outlook.com>
2017-12-07 22:39:25 -0800

    Added solution for day 1 part 2

--- 2017/day01/index.js
+++ 2017/day01/index.js
@@ -4,22 +4,19 @@ if (process.argv.length < 2) {
   process.exit(-1)
 }
 
-function sumString (str) {
+function sumString (str, offset) {
   let sum = 0
   const length = str.length
 
-  for (let i = 0; i < length - 1; i++) {
-    if (str[i] === str[i + 1]) {
+  for (let i = 0; i < length; i++) {
+    if (str[i] === str[(i + offset) % length]) {
       sum += str[i] - '0'
     }
-  }
-
-  if (str[length - 1] === str[0]) {
-    sum += str[length - 1] - '0'
   }
 
   return sum
 }
 
-const fileContent = fs.readFileSync(process.argv[2], 'utf8')
-console.log(sumString(fileContent.trim()))
+const fileContent = fs.readFileSync(process.argv[2], 'utf8').trim()
+console.log(`Part 1: ${sumString(fileContent, 1)}`)
+console.log(`Part 2: ${sumString(fileContent, Math.floor(fileContent.length / 2))}`)
