427b8bf012881b6df590448e636cb44eaef31d5a
Kyle Farnung <kfarnung@outlook.com>
2018-02-03 21:56:22 -0800

    Perf optimization - Day 15
    * Converted `generateValues` generator function to a class
    * Converted `Math.pow` to bitwise left shift
    * Converted run function to a generator so results print as they are
      generated

--- 2017/day15/index.js
+++ 2017/day15/index.js
@@ -1,38 +1,43 @@
 const fs = require('fs');
 
+class Generator {
+  constructor (startingValue, factor, requiredMultiple = 1) {
+    this._previousValue = startingValue;
+    this._factor = factor;
+    this._requiredMultiple = requiredMultiple;
+  }
+
+  next () {
+    while (true) {
+      const value = this._previousValue = (this._previousValue * this._factor) % 2147483647;
+
+      const requiredMultiple = this._requiredMultiple;
+      if (requiredMultiple === 1 || value % requiredMultiple === 0) {
+        return value;
+      }
+    }
+  }
+}
+
 class Day15 {
   static parseInput (str) {
     const search = /[0-9]+/g;
     return str.match(search).map((match) => Number.parseInt(match));
   }
 
-  static * generateValues (startingValue, factor, requiredMultiple = 1) {
-    let previousValue = startingValue;
-    while (true) {
-      previousValue = (previousValue * factor) % 2147483647;
-
-      if (previousValue % requiredMultiple === 0) {
-        yield previousValue;
-      }
-    }
-  }
-
   static areBitsEqual (val1, val2, numBits) {
-    const mask = Math.pow(2, numBits) - 1;
+    const mask = (1 << numBits) - 1;
     return (val1 & mask) === (val2 & mask);
   }
 
   static countMatches (startingValueA, startingValueB, numIterations, requiredMultipleA = 1, requiredMultipleB = 1) {
-    const generatorA = this.generateValues(startingValueA, 16807, requiredMultipleA);
-    const generatorB = this.generateValues(startingValueB, 48271, requiredMultipleB);
+    const generatorA = new Generator(startingValueA, 16807, requiredMultipleA);
+    const generatorB = new Generator(startingValueB, 48271, requiredMultipleB);
 
     let count = 0;
 
     for (let i = 0; i < numIterations; i++) {
-      const valueA = generatorA.next().value;
-      const valueB = generatorB.next().value;
-
-      if (this.areBitsEqual(valueA, valueB, 16)) {
+      if (this.areBitsEqual(generatorA.next(), generatorB.next(), 16)) {
         count++;
       }
     }
@@ -40,14 +45,12 @@ class Day15 {
     return count;
   }
 
-  static run (input) {
+  static * run (input) {
     const fileContent = fs.readFileSync(input, 'utf8');
     const [ startingValueA, startingValueB ] = this.parseInput(fileContent);
 
-    return [
-      this.countMatches(startingValueA, startingValueB, 40000000),
-      this.countMatches(startingValueA, startingValueB, 5000000, 4, 8)
-    ];
+    yield this.countMatches(startingValueA, startingValueB, 40000000);
+    yield this.countMatches(startingValueA, startingValueB, 5000000, 4, 8);
   }
 }
 
