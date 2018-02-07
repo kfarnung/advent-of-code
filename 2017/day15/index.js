8c81a3cb163b15a947824494ac8aceca7a844c0f
Kyle Farnung <kfarnung@outlook.com>
2018-02-06 21:35:14 -0800

    Move all computation to WASM for Day 15

--- 2017/day15/index.js
+++ 2017/day15/index.js
@@ -7,43 +7,14 @@ const readyPromise = new Promise((resolve) => {
   };
 });
 
-class Generator {
-  constructor (startingValue, factor, requiredMultiple = 1) {
-    this._previousValue = startingValue;
-    this._factor = factor;
-    this._requiredMultiple = requiredMultiple;
-  }
-
-  next () {
-    this._previousValue = wasmDay15._calculateNext(this._previousValue, this._factor, this._requiredMultiple);
-    return this._previousValue;
-  }
-}
-
 class Day15 {
   static parseInput (str) {
     const search = /[0-9]+/g;
     return str.match(search).map((match) => Number.parseInt(match));
   }
 
-  static areBitsEqual (val1, val2, numBits) {
-    const mask = (1 << numBits) - 1;
-    return (val1 & mask) === (val2 & mask);
-  }
-
   static countMatches (startingValueA, startingValueB, numIterations, requiredMultipleA = 1, requiredMultipleB = 1) {
-    const generatorA = new Generator(startingValueA, 16807, requiredMultipleA);
-    const generatorB = new Generator(startingValueB, 48271, requiredMultipleB);
-
-    let count = 0;
-
-    for (let i = 0; i < numIterations; i++) {
-      if (this.areBitsEqual(generatorA.next(), generatorB.next(), 16)) {
-        count++;
-      }
-    }
-
-    return count;
+    return wasmDay15._countMatches(startingValueA, startingValueB, numIterations, requiredMultipleA, requiredMultipleB);
   }
 
   static * runScenarios (input) {
--- 2017/wasm/day15.c
+++ 2017/wasm/day15.c
@@ -1,16 +1,48 @@
 #include <emscripten.h>
+#include <stdbool.h>
 #include <stdint.h>
 
-uint32_t EMSCRIPTEN_KEEPALIVE calculateNext(
-    uint32_t previousValue,
-    uint32_t factor,
-    uint32_t requiredMultiple)
+typedef struct GeneratorData
+{
+    uint32_t currentValue;
+    uint32_t factor;
+    uint32_t requiredMultiple;
+} GeneratorData;
+
+void calculateNext(GeneratorData* data)
 {
     do
     {
-        previousValue = ((uint64_t)previousValue * factor) % 2147483647;
+        data->currentValue = ((uint64_t)data->currentValue * data->factor) % 2147483647;
     }
-    while (requiredMultiple != 1 && previousValue % requiredMultiple != 0);
+    while (data->requiredMultiple != 1 && data->currentValue % data->requiredMultiple != 0);
+}
 
-    return previousValue;
+bool areBitsEqual(uint32_t val1, uint32_t val2, uint32_t numBits) {
+    uint32_t mask = (1 << numBits) - 1;
+    return (val1 & mask) == (val2 & mask);
+}
+
+uint32_t EMSCRIPTEN_KEEPALIVE countMatches(
+    uint32_t startingValueA,
+    uint32_t startingValueB,
+    uint32_t numIterations,
+    uint32_t requiredMultipleA,
+    uint32_t requiredMultipleB)
+{
+    GeneratorData generatorA = { startingValueA, 16807, requiredMultipleA };
+    GeneratorData generatorB = { startingValueB, 48271, requiredMultipleB };
+
+    uint32_t count = 0;
+
+    for (uint32_t i = 0; i < numIterations; i++) {
+        calculateNext(&generatorA);
+        calculateNext(&generatorB);
+
+        if (areBitsEqual(generatorA.currentValue, generatorB.currentValue, 16)) {
+            count++;
+        }
+    }
+
+    return count;
 }
