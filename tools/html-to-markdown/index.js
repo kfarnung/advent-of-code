c6b6ce8b400eadcd3a7c4017519f8f2e309c7dd5
Kyle Farnung <kfarnung@outlook.com>
2018-11-19 00:11:47 -0800

    Update html-to-markdown to process the whole page

--- tools/html-to-markdown/index.js
+++ tools/html-to-markdown/index.js
@@ -1,6 +1,47 @@
+const cheerio = require('cheerio');
 const fs = require('fs');
-const toMarkdown = require('to-markdown');
+const gfm = require('turndown-plugin-gfm').gfm;
+const os = require('os');
+const TurndownService = require('turndown');
+const yargs = require('yargs');
 
-const htmlContent = fs.readFileSync(process.argv[2], 'utf8');
-const markdownContent = toMarkdown(htmlContent, { gfm: true });
-fs.writeFileSync(process.argv[3], markdownContent);
+const year = yargs.argv.year;
+const day = yargs.argv.day;
+
+const htmlContent = fs.readFileSync(yargs.argv._[0], 'utf8');
+const $ = cheerio.load(htmlContent);
+
+const turndownService = new TurndownService({
+  headingStyle: 'atx'
+});
+turndownService.use(gfm);
+
+function stripHyphens (str) {
+  const regex = /^--- (.+) ---$/;
+  return str.replace(regex, (match, p1) => {
+    return p1;
+  });
+}
+
+const newDoc = $('<div>');
+newDoc.append('<h1>');
+newDoc.append($('<p>').text(`http://adventofcode.com/${year}/day/${day}`));
+newDoc.append($('<h2>').text('Description'));
+
+$('article').each((index, element) => {
+  const headingElement = $('h2', element);
+  let heading = stripHyphens(headingElement.text());
+
+  if (index === 0) {
+    $('h1', newDoc).text(heading);
+    heading = 'Part One';
+  }
+
+  headingElement.replaceWith($('<h3>').text(heading));
+
+  newDoc.append(element);
+});
+
+const markdownContent = turndownService.turndown(newDoc.html()).concat(os.EOL);
+
+fs.writeFileSync(yargs.argv._[1], markdownContent);
