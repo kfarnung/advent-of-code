f4cd1d3b5c7a53348c460d4a7d576a645de0dfbc
Kyle Farnung <kfarnung@outlook.com>
2018-12-01 08:53:59 -0800

    Cleaning up code
    * Take advantage of integer parsing to handle the sign
    * Use reduce for simple summation

--- 2018/day01/day01.py
+++ 2018/day01/day01.py
@@ -4,24 +4,12 @@ Implementation for Advent of Code Day 1.
 https://adventofcode.com/2018/day/1
 """
 
-def get_new_frequency(frequency, modification):
-    """Calculates the frequency for a given input."""
-    operation = modification[0]
-    magnitude = int(modification[1:])
-    if operation == '+':
-        return frequency + magnitude
-
-    return frequency - magnitude
-
-
 def calculate_frequency(inputs):
     """Calculates the frequency for a given set of inputs."""
-    frequency = 0
-
-    for modification in inputs:
-        frequency = get_new_frequency(frequency, modification)
-
-    return frequency
+    return reduce(
+        lambda prev, current: prev + int(current),
+        inputs,
+        0)
 
 def calculate_frequency_two_match(inputs):
     """Calculates the frequency for a given set of inputs."""
@@ -31,7 +19,7 @@ def calculate_frequency_two_match(inputs):
 
     while True:
         for modification in inputs:
-            frequency = get_new_frequency(frequency, modification)
+            frequency += int(modification)
 
             if frequency in seen:
                 return frequency
