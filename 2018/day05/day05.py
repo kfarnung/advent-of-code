cdbc5c44532fa7332e7ad89a7be61b0a1a12bf5c
Kyle Farnung <kfarnung@outlook.com>
2018-12-04 22:00:48 -0800

    Solution for 2018 day 5

---
+++ 2018/day05/day05.py
@@ -0,0 +1,60 @@
+"""
+Implementation for Advent of Code Day 5.
+
+https://adventofcode.com/2018/day/5
+"""
+
+import string
+import sys
+
+def _does_react(unit1, unit2):
+    return abs(ord(unit1) - ord(unit2)) == 32
+
+def _reduce_polymer(polymer):
+    characters = list(polymer)
+    index = 1
+
+    while index < len(characters):
+        if _does_react(characters[index - 1], characters[index]):
+            del characters[index]
+            del characters[index - 1]
+
+            # Back up a little bit to catch any new matches
+            index = max(index - 2, 1)
+        else:
+            index += 1
+
+    return characters
+
+def _remove_unit_type(polymer, unit_type):
+    unit_type = unit_type.lower()
+    return [x for x in polymer if x.lower() != unit_type]
+
+def run_part1(file_content):
+    """Implmentation for Part 1."""
+    return len(_reduce_polymer(file_content))
+
+def run_part2(file_content):
+    """Implmentation for Part 2."""
+    size = sys.maxint
+
+    for unit_type in string.ascii_lowercase:
+        reduced = _remove_unit_type(file_content, unit_type)
+        reduced = _reduce_polymer(reduced)
+        size = min(size, len(reduced))
+
+    return size
+
+if __name__ == "__main__":
+    def run(input_path):
+        """The main function."""
+        with open(input_path, 'r') as input_file:
+            file_content = input_file.read().strip()
+            print "Part 1: {}".format(run_part1(file_content))
+            print "Part 2: {}".format(run_part2(file_content))
+
+    if len(sys.argv) < 2:
+        print "Usage: python {} <input>".format(sys.argv[0])
+        sys.exit(1)
+
+    run(sys.argv[1])
---
+++ 2018/day05/test_day05.py
@@ -0,0 +1,19 @@
+"""
+Tests for Advent of Code Day 5.
+
+https://adventofcode.com/2018/day/5
+"""
+
+from .day05 import run_part1, run_part2
+
+def test_part1():
+    """Tests for Part 1."""
+    assert run_part1('aA') == 0
+    assert run_part1('abBA') == 0
+    assert run_part1('abAB') == 4
+    assert run_part1('aabAAB') == 6
+    assert run_part1('dabAcCaCBAcCcaDA') == 10
+
+def test_part2():
+    """Tests for Part 2."""
+    assert run_part2('dabAcCaCBAcCcaDA') == 4
