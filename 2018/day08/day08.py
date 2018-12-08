ab18d2d5665ae658154bec6c66fd4a5b204f0e19
Kyle Farnung <kfarnung@outlook.com>
2018-12-07 21:32:04 -0800

    Solution for 2018 day 8

---
+++ 2018/day08/day08.py
@@ -0,0 +1,73 @@
+"""
+Implementation for Advent of Code Day 8.
+
+https://adventofcode.com/2018/day/8
+"""
+
+class _TreeNode(object):
+    def __init__(self):
+        self.children = []
+        self.metadata = []
+
+def _build_tree(inputs):
+    child_count = inputs.next()
+    metadata_count = inputs.next()
+    node = _TreeNode()
+
+    for _ in xrange(child_count):
+        node.children.append(_build_tree(inputs))
+
+    for _ in xrange(metadata_count):
+        node.metadata.append(inputs.next())
+
+    return node
+
+def _sum_metadata(node):
+    metadata_sum = 0
+
+    for child in node.children:
+        metadata_sum += _sum_metadata(child)
+
+    metadata_sum += sum(node.metadata)
+    return metadata_sum
+
+def _node_value(node):
+    value = 0
+
+    if node.children:
+        for index in node.metadata:
+            index -= 1
+            if index < len(node.children):
+                value += _node_value(node.children[index])
+    else:
+        value += sum(node.metadata)
+
+    return value
+
+def run_part1(file_content):
+    """Implmentation for Part 1."""
+    numbers = (int(number) for number in file_content.split())
+    root = _build_tree(numbers)
+    return _sum_metadata(root)
+
+def run_part2(file_content):
+    """Implmentation for Part 2."""
+    numbers = (int(number) for number in file_content.split())
+    root = _build_tree(numbers)
+    return _node_value(root)
+
+if __name__ == "__main__":
+    import sys
+
+    def run(input_path):
+        """The main function."""
+        with open(input_path, 'r') as input_file:
+            file_content = input_file.read().strip()
+            print "Part 1: {}".format(run_part1(file_content))
+            print "Part 2: {}".format(run_part2(file_content))
+
+    if len(sys.argv) < 2:
+        print "Usage: python {} <input>".format(sys.argv[0])
+        sys.exit(1)
+
+    run(sys.argv[1])
---
+++ 2018/day08/test_day08.py
@@ -0,0 +1,27 @@
+"""
+Tests for Advent of Code Day 8.
+
+https://adventofcode.com/2018/day/8
+"""
+
+from os import path
+from .day08 import run_part1, run_part2
+
+_CURRENT_FILE_DIR = path.dirname(__file__)
+_TEST_DATA = '2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2'
+
+def test_part1():
+    """Tests for Part 1."""
+    assert run_part1(_TEST_DATA) == 138
+
+    with open(path.join(_CURRENT_FILE_DIR, 'input'), 'r') as input_file:
+        file_content = input_file.read().strip()
+        assert run_part1(file_content) == 42472
+
+def test_part2():
+    """Tests for Part 2."""
+    assert run_part2(_TEST_DATA) == 66
+
+    with open(path.join(_CURRENT_FILE_DIR, 'input'), 'r') as input_file:
+        file_content = input_file.read().strip()
+        assert run_part2(file_content) == 21810
