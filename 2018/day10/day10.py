3f300dc0dc986c94f0ee8928df89b16dc0ba2eb0
Kyle Farnung <kfarnung@outlook.com>
2018-12-09 23:14:03 -0800

    Solution for 2018 day 10

---
+++ 2018/day10/day10.py
@@ -0,0 +1,150 @@
+"""
+Implementation for Advent of Code Day 10.
+
+https://adventofcode.com/2018/day/10
+"""
+
+import re
+
+_ENTRY_REGEX = re.compile(r'^position=< *(-?\d+), *(-?\d+)> velocity=< *(-?\d+), *(-?\d+)>$')
+
+class _Point2D(object):
+    """Represents a point in 2D space."""
+    def __init__(self, coord_x, coord_y):
+        self.coord_x = coord_x
+        self.coord_y = coord_y
+
+    def __eq__(self, other):
+        return self.coord_x == other.coord_x and self.coord_y == other.coord_y
+
+    def __hash__(self):
+        return hash((self.coord_x, self.coord_y))
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def add(self, other):
+        """Adds the given point to the current instance."""
+        self.coord_x += other.coord_x
+        self.coord_y += other.coord_y
+
+    def subtract(self, other):
+        """Subtracts the given point from the current instance."""
+        self.coord_x -= other.coord_x
+        self.coord_y -= other.coord_y
+
+class _LightPoint(object):
+    """Represents a moving point of light in the sky."""
+    def __init__(self, position, velocity):
+        self.current_position = position
+        self.velocity = velocity
+
+    def step_backward(self):
+        """Apply the inverse velocity to step backward in time."""
+        self.current_position.subtract(self.velocity)
+
+    def step_forward(self):
+        """Apply the velocity to step forward in time."""
+        self.current_position.add(self.velocity)
+
+class _LightPointSystem(object):
+    """Represents a system of light points in the sky."""
+    def __init__(self, points):
+        self.points = points
+
+    def get_area(self):
+        """Gets the area of the rectange that bounds the points."""
+        upper_left, lower_right = self._get_bounds()
+        length_x = lower_right.coord_x - upper_left.coord_x
+        length_y = lower_right.coord_y - upper_left.coord_y
+        return length_x * length_y
+
+    def plot_points(self):
+        """Plot the points in coordinate space."""
+        upper_left, lower_right = self._get_bounds()
+        points = set(point.current_position for point in self.points)
+        lines = []
+
+        for coord_y in xrange(upper_left.coord_y, lower_right.coord_y + 1):
+            line = []
+
+            for coord_x in xrange(upper_left.coord_x, lower_right.coord_x + 1):
+                point = _Point2D(coord_x, coord_y)
+                line.append('#' if point in points else ' ')
+
+            lines.append(''.join(line))
+
+        return '\n'.join(lines)
+
+    def step_backward(self):
+        """Step the system of points backward in time."""
+        for point in self.points:
+            point.step_backward()
+
+    def step_forward(self):
+        """Step the system of points forward in time."""
+        for point in self.points:
+            point.step_forward()
+
+    def _get_bounds(self):
+        """Gets the rectange that bounds the points."""
+        min_x = self.points[0].current_position.coord_x
+        min_y = self.points[0].current_position.coord_y
+        max_x = self.points[0].current_position.coord_x
+        max_y = self.points[0].current_position.coord_y
+
+        for point in self.points:
+            min_x = min(min_x, point.current_position.coord_x)
+            min_y = min(min_y, point.current_position.coord_y)
+            max_x = max(max_x, point.current_position.coord_x)
+            max_y = max(max_y, point.current_position.coord_y)
+
+        return _Point2D(min_x, min_y), _Point2D(max_x, max_y)
+
+def _parse_entry(input_str):
+    """Parse the entry that contains the starting point and velocity."""
+    match = _ENTRY_REGEX.match(input_str)
+    if not match:
+        raise Exception('Invalid input string')
+
+    return _LightPoint(
+        _Point2D(int(match.group(1)), int(match.group(2))),
+        _Point2D(int(match.group(3)), int(match.group(4))),
+    )
+
+def find_message(file_content):
+    """Implmentation for Part 1."""
+    point_system = _LightPointSystem([_parse_entry(entry) for entry in file_content])
+    total_area = point_system.get_area()
+    seconds = 0
+
+    while True:
+        point_system.step_forward()
+        new_area = point_system.get_area()
+        seconds += 1
+
+        if new_area > total_area:
+            point_system.step_backward()
+            seconds -= 1
+            break
+
+        total_area = new_area
+
+    return point_system.plot_points(), seconds
+
+if __name__ == "__main__":
+    import sys
+
+    def run(input_path):
+        """The main function."""
+        with open(input_path, 'r') as input_file:
+            message, seconds = find_message(input_file.readlines())
+            print "Part 1:"
+            print message
+            print "Part 2: {}".format(seconds)
+
+    if len(sys.argv) < 2:
+        print "Usage: python {} <input>".format(sys.argv[0])
+        sys.exit(1)
+
+    run(sys.argv[1])
---
+++ 2018/day10/test_day10.py
@@ -0,0 +1,76 @@
+"""
+Tests for Advent of Code Day 10.
+
+https://adventofcode.com/2018/day/10
+"""
+
+from os import path
+from .day10 import find_message
+
+_CURRENT_FILE_DIR = path.dirname(__file__)
+_TEST_DATA = [
+    'position=< 9,  1> velocity=< 0,  2>',
+    'position=< 7,  0> velocity=<-1,  0>',
+    'position=< 3, -2> velocity=<-1,  1>',
+    'position=< 6, 10> velocity=<-2, -1>',
+    'position=< 2, -4> velocity=< 2,  2>',
+    'position=<-6, 10> velocity=< 2, -2>',
+    'position=< 1,  8> velocity=< 1, -1>',
+    'position=< 1,  7> velocity=< 1,  0>',
+    'position=<-3, 11> velocity=< 1, -2>',
+    'position=< 7,  6> velocity=<-1, -1>',
+    'position=<-2,  3> velocity=< 1,  0>',
+    'position=<-4,  3> velocity=< 2,  0>',
+    'position=<10, -3> velocity=<-1,  1>',
+    'position=< 5, 11> velocity=< 1, -2>',
+    'position=< 4,  7> velocity=< 0, -1>',
+    'position=< 8, -2> velocity=< 0,  1>',
+    'position=<15,  0> velocity=<-2,  0>',
+    'position=< 1,  6> velocity=< 1,  0>',
+    'position=< 8,  9> velocity=< 0, -1>',
+    'position=< 3,  3> velocity=<-1,  1>',
+    'position=< 0,  5> velocity=< 0, -1>',
+    'position=<-2,  2> velocity=< 2,  0>',
+    'position=< 5, -2> velocity=< 1,  2>',
+    'position=< 1,  4> velocity=< 2,  1>',
+    'position=<-2,  7> velocity=< 2, -2>',
+    'position=< 3,  6> velocity=<-1, -1>',
+    'position=< 5,  0> velocity=< 1,  0>',
+    'position=<-6,  0> velocity=< 2,  0>',
+    'position=< 5,  9> velocity=< 1, -2>',
+    'position=<14,  7> velocity=<-2,  0>',
+    'position=<-3,  6> velocity=< 2, -1>',
+]
+_TEST_EXPECTED = (
+    '#   #  ###\n'
+    '#   #   # \n'
+    '#   #   # \n'
+    '#####   # \n'
+    '#   #   # \n'
+    '#   #   # \n'
+    '#   #   # \n'
+    '#   #  ###'
+)
+_INPUT_EXPECTED = (
+    '######  ######   ####   #####    ####    ####    ####      ###\n'
+    '     #       #  #    #  #    #  #    #  #    #  #    #      # \n'
+    '     #       #  #       #    #  #       #       #           # \n'
+    '    #       #   #       #    #  #       #       #           # \n'
+    '   #       #    #       #####   #       #       #           # \n'
+    '  #       #     #       #    #  #  ###  #  ###  #           # \n'
+    ' #       #      #       #    #  #    #  #    #  #           # \n'
+    '#       #       #       #    #  #    #  #    #  #       #   # \n'
+    '#       #       #    #  #    #  #   ##  #   ##  #    #  #   # \n'
+    '######  ######   ####   #####    ### #   ### #   ####    ###  '
+)
+
+def test_find_message():
+    """Tests for Part 1 """
+    message, seconds = find_message(_TEST_DATA)
+    assert message == _TEST_EXPECTED
+    assert seconds == 3
+
+    with open(path.join(_CURRENT_FILE_DIR, 'input'), 'r') as input_file:
+        message, seconds = find_message(input_file.readlines())
+        assert message == _INPUT_EXPECTED
+        assert seconds == 10886
