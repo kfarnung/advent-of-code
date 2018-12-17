259a17528c20cccb6aa428db37e2a951221d061d
Kyle Farnung <kfarnung@outlook.com>
2018-12-16 20:56:48 -0800

    Solution for 2018 day 17

---
+++ 2018/day17/day17.py
@@ -0,0 +1,147 @@
+"""
+Implementation for Advent of Code Day 17.
+
+https://adventofcode.com/2018/day/17
+"""
+
+import re
+from collections import defaultdict
+from itertools import repeat
+from operator import itemgetter
+
+_SPAN_REGEX = re.compile(r'^([xy])=(\d+), [xy]=(\d+)..(\d+)$')
+
+class Grid(object):
+    """Represents a grid of water, clay, and sand."""
+    def __init__(self, clay_positions):
+        self.ground = defaultdict(repeat('.').next)
+        for clay in clay_positions:
+            self.ground[clay] = '#'
+
+        self.min_y = min(self.ground.iterkeys(), key=itemgetter(1))[1]
+        self.max_y = max(self.ground.iterkeys(), key=itemgetter(1))[1]
+
+    def __str__(self):
+        min_x = min(self.ground.iterkeys(), key=itemgetter(0))[0]
+        max_x = max(self.ground.iterkeys(), key=itemgetter(0))[0]
+
+        return '\n'.join(''.join(self.ground[(coord_x, coord_y)]
+                                 for coord_x in xrange(min_x, max_x + 1))
+                         for coord_y in xrange(self.min_y, self.max_y + 1))
+
+    def fill(self):
+        """Fill the grid with water."""
+        self._flow((500, self.min_y))
+
+    def get_water_count(self):
+        """Get the total number of water tiles."""
+        return reduce(
+            lambda prev, current: prev + 1 if current == '~' or current == '|' else prev,
+            self.ground.itervalues(),
+            0
+        )
+
+    def get_settled_count(self):
+        """Get the total number of settled water tiles."""
+        return reduce(
+            lambda prev, current: prev + 1 if current == '~' else prev,
+            self.ground.itervalues(),
+            0
+        )
+
+    def _flow(self, position):
+        self.ground[position] = '|'
+
+        if position[1] == self.max_y:
+            return
+
+        down = (position[0], position[1] + 1)
+        down_tile = self.ground[down]
+        if down_tile == '|':
+            # Combining with other flowing water.
+            return
+        elif down_tile == '.':
+            self._flow(down)
+
+        # Try to fill left and right
+        left = position
+        while self._can_flow_horizontal(left):
+            self.ground[left] = '|'
+            left = (left[0] - 1, left[1])
+
+        right = position
+        while self._can_flow_horizontal(right):
+            self.ground[right] = '|'
+            right = (right[0] + 1, right[1])
+
+        if self.ground[left] == '#' and self.ground[right] == '#':
+            self._mark_settled(left, right)
+            return
+
+        if self.ground[left] == '.':
+            self._flow(left)
+
+        if self.ground[right] == '.':
+            self._flow(right)
+
+    def _mark_settled(self, left, right):
+        assert left[1] == right[1]
+
+        for coord_x in xrange(left[0] + 1, right[0]):
+            self.ground[(coord_x, left[1])] = '~'
+
+    def _can_flow_horizontal(self, position):
+        down = (position[0], position[1] + 1)
+        current_tile = self.ground[position]
+        if current_tile != '.' and current_tile != '|':
+            return False
+
+        down_tile = self.ground[down]
+        if down_tile != '#' and down_tile != '~':
+            return False
+
+        return True
+
+def _parse_clay(file_content):
+    clay = []
+    for line in file_content:
+        match = _SPAN_REGEX.match(line)
+        if not match:
+            raise Exception('Invalid line')
+
+        flipped = match.group(1) == 'y'
+        first = int(match.group(2))
+        clay += [(first, second) if not flipped else (second, first)
+                 for second in xrange(int(match.group(3)), int(match.group(4)) + 1)]
+
+    return clay
+
+def run_part1(file_content):
+    """Implmentation for Part 1."""
+    grid = Grid(_parse_clay(file_content))
+    grid.fill()
+    return grid.get_water_count()
+
+def run_part2(file_content):
+    """Implmentation for Part 2."""
+    grid = Grid(_parse_clay(file_content))
+    grid.fill()
+    return grid.get_settled_count()
+
+if __name__ == "__main__":
+    import sys
+
+    def run(argv1):
+        """The main function."""
+        with open(argv1, 'r') as input_file:
+            file_content = input_file.readlines()
+            print "Part 1: {}".format(run_part1(file_content))
+            print "Part 2: {}".format(run_part2(file_content))
+
+    if len(sys.argv) < 2:
+        print "Usage: python {} <input>".format(sys.argv[0])
+        sys.exit(1)
+
+    # It would be better to use a separate stack given the depth required.
+    sys.setrecursionlimit(10000)
+    run(sys.argv[1])
---
+++ 2018/day17/test_day17.py
@@ -0,0 +1,51 @@
+"""
+Tests for Advent of Code Day 17.
+
+https://adventofcode.com/2018/day/17
+"""
+
+import sys
+from os import path
+from .day17 import run_part1, run_part2
+
+_CURRENT_FILE_DIR = path.dirname(__file__)
+_TEST_DATA = [
+    'x=495, y=2..7',
+    'y=7, x=495..501',
+    'x=501, y=3..7',
+    'x=498, y=2..4',
+    'x=506, y=1..2',
+    'x=498, y=10..13',
+    'x=504, y=10..13',
+    'y=13, x=498..504',
+]
+_TEST_DATA_2 = [
+    'x=485, y=2..4',
+    'x=490, y=5..16',
+    'y=16, x=490..513',
+    'x=513, y=4..16',
+    'x=499, y=7..10',
+    'y=7, x=499..508',
+    'y=10, x=499..508',
+    'x=508, y=7..10',
+]
+
+def test_part1():
+    """Tests for Part 1."""
+    assert run_part1(_TEST_DATA) == 57
+    assert run_part1(_TEST_DATA_2) == 240
+
+    sys.setrecursionlimit(10000)
+    with open(path.join(_CURRENT_FILE_DIR, 'input'), 'r') as input_file:
+        file_content = input_file.readlines()
+        assert run_part1(file_content) == 29063
+
+def test_part2():
+    """Tests for Part 2."""
+    assert run_part2(_TEST_DATA) == 29
+    assert run_part2(_TEST_DATA_2) == 202
+
+    sys.setrecursionlimit(10000)
+    with open(path.join(_CURRENT_FILE_DIR, 'input'), 'r') as input_file:
+        file_content = input_file.readlines()
+        assert run_part2(file_content) == 23811
