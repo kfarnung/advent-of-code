e2ac428d3b8481fb90efc37c2e88f27f3a2ef9e5
Kyle Farnung <kfarnung@outlook.com>
2019-11-23 19:38:44 -0800

    Solution for 2019 day 2

---
+++ 2019/src/bin/day02.rs
@@ -0,0 +1,24 @@
+use aoc2019::day02;
+use std::env;
+use std::fs;
+use std::process;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("Missing required arguments");
+        process::exit(1);
+    }
+
+    let contents = fs::read_to_string(&args[1])
+        .expect("Something went wrong");
+
+    let initial_memory = contents
+        .trim()
+        .split(',')
+        .map(|i| i.parse::<usize>().unwrap())
+        .collect();
+
+    println!("Part 1: {}", day02::part1(&initial_memory));
+    println!("Part 2: {}", day02::part2(&initial_memory));
+}
---
+++ 2019/src/day02.rs
@@ -0,0 +1,87 @@
+pub fn part1(initial_memory: &Vec<usize>) -> usize {
+    let memory = run_program(initial_memory, Some(12), Some(2));
+    return memory[0];
+}
+
+pub fn part2(initial_memory: &Vec<usize>) -> usize {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let memory = run_program(initial_memory, Some(noun), Some(verb));
+            if memory[0] == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    
+    panic!("We didn't find it!");
+}
+
+fn run_program(initial_memory: &Vec<usize>, noun: Option<usize>, verb: Option<usize>) -> Vec<usize> {
+    let mut memory = initial_memory.clone();
+    let mut ip = 0;
+
+    match noun {
+        Some(x) => memory[1] = x,
+        None => (),
+    }
+
+    match verb {
+        Some(x) => memory[2] = x,
+        None => (),
+    }
+
+    loop {
+        match memory[ip] {
+            1 => ip = do_add(&mut memory, ip),
+            2 => ip = do_multiply(&mut memory, ip),
+            99 => break,
+            _ => panic!("Unexpected opcode!"),
+        }
+    }
+
+    return memory;
+}
+
+fn do_add(memory: &mut Vec<usize>, ip: usize) -> usize {
+    let a = memory[ip + 1];
+    let b = memory[ip + 2];
+    let r = memory[ip + 3];
+
+    memory[r] = memory[a] + memory[b];
+    return ip + 4;
+}
+
+fn do_multiply(memory: &mut Vec<usize>, ip: usize) -> usize {
+    let a = memory[ip + 1];
+    let b = memory[ip + 2];
+    let r = memory[ip + 3];
+
+    memory[r] = memory[a] * memory[b];
+    return ip + 4;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_run_program() {
+        let cases = vec![
+            (
+                vec![1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
+                vec![3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50],
+            ),
+            (vec![1, 0, 0, 0, 99], vec![2, 0, 0, 0, 99]),
+            (vec![2, 3, 0, 3, 99], vec![2, 3, 0, 6, 99]),
+            (vec![2, 4, 4, 5, 99, 0], vec![2, 4, 4, 5, 99, 9801]),
+            (
+                vec![1, 1, 1, 4, 99, 5, 6, 0, 99],
+                vec![30, 1, 1, 4, 2, 5, 6, 0, 99],
+            ),
+        ];
+
+        for case in cases {
+            assert_eq!(run_program(&case.0, None, None), case.1);
+        }
+    }
+}
