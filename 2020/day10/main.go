8cddc867f31586a1d3a3715f656635bc74e4b211
Kyle Farnung <kfarnung@outlook.com>
2020-12-09 23:14:56 -0800

    Solution for 2020 day 10

---
+++ 2020/day10/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/kfarnung/advent-of-code/2020/lib"
+)
+
+func countPaths(nums []int, memo map[int]int64, index int) int64 {
+	if value, ok := memo[index]; ok {
+		return value
+	}
+
+	if index == len(nums)-1 {
+		return 1
+	}
+
+	var count int64
+	current := nums[index]
+	for i := index + 1; i < len(nums) && nums[i]-current <= 3; i++ {
+		count += countPaths(nums, memo, i)
+	}
+
+	memo[index] = count
+	return count
+}
+
+func parseInput(lines []string) ([]int, error) {
+	nums := make([]int, len(lines))
+
+	for i, line := range lines {
+		num, err := lib.ParseInt(line)
+		if err != nil {
+			return nil, err
+		}
+
+		nums[i] = num
+	}
+
+	nums = append(nums, 0)
+	sort.Ints(nums)
+	highest := nums[len(nums)-1]
+	nums = append(nums, highest+3)
+
+	return nums, nil
+}
+
+func part1(nums []int) int {
+	var lastNum int
+	diffCounts := make([]int, 4)
+	for _, num := range nums {
+		diff := num - lastNum
+		diffCounts[diff]++
+		lastNum = num
+	}
+
+	return diffCounts[1] * diffCounts[3]
+}
+
+func part2(nums []int) int64 {
+	memo := make(map[int]int64)
+	return countPaths(nums, memo, 0)
+}
+
+func main() {
+	name := os.Args[1]
+	lines, err := lib.LoadFileLines(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	nums, err := parseInput(lines)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Part 1: %d\n", part1(nums))
+	fmt.Printf("Part 2: %d\n", part2(nums))
+}
---
+++ 2020/day10/main_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/kfarnung/advent-of-code/2020/lib"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestExample(t *testing.T) {
+	lines := []string{
+		"16",
+		"10",
+		"15",
+		"5",
+		"1",
+		"11",
+		"7",
+		"19",
+		"6",
+		"12",
+		"4",
+	}
+
+	nums, err := parseInput(lines)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 35, part1(nums))
+	assert.Equal(t, int64(8), part2(nums))
+}
+
+func TestExample2(t *testing.T) {
+	lines := []string{
+		"28",
+		"33",
+		"18",
+		"42",
+		"31",
+		"14",
+		"46",
+		"20",
+		"48",
+		"47",
+		"24",
+		"23",
+		"49",
+		"45",
+		"19",
+		"38",
+		"39",
+		"11",
+		"1",
+		"32",
+		"25",
+		"35",
+		"8",
+		"17",
+		"7",
+		"9",
+		"4",
+		"2",
+		"34",
+		"10",
+		"3",
+	}
+
+	nums, err := parseInput(lines)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 220, part1(nums))
+	assert.Equal(t, int64(19208), part2(nums))
+}
+
+func TestInput(t *testing.T) {
+	name := lib.GetTestFilePath("input.txt")
+	lines, err := lib.LoadFileLines(name)
+	assert.NoError(t, err)
+
+	nums, err := parseInput(lines)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 2482, part1(nums))
+	assert.Equal(t, int64(96717311574016), part2(nums))
+}
