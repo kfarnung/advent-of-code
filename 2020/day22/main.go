5b3d942d4effa526f681fa5047e72e40c13f07f0
Kyle Farnung <kfarnung@outlook.com>
2020-12-22 09:02:33 -0800

    [2020 day22] Optimize the code to reduce runtime
    A few optimizations:
    
    - Remove unnecessary type (saves ~0.5 seconds)
    - Grow the string builder (saves ~0.5 seconds)
    - Avoid Printf and friends (saves ~0.5 seconds)

--- 2020/day22/main.go
+++ 2020/day22/main.go
@@ -9,91 +9,69 @@ import (
 	"github.com/kfarnung/advent-of-code/2020/lib"
 )
 
-type spaceCard int
+type spaceCardDeck []int
 
-func (s spaceCard) String() string {
-	return fmt.Sprintf("%d", s)
-}
-
-type spaceCardDeck []spaceCard
-
-func (s spaceCardDeck) Clone(cardCount int) spaceCardDeck {
+func (s spaceCardDeck) clone(cardCount int) spaceCardDeck {
 	newDeck := make(spaceCardDeck, cardCount)
-	for i, card := range s {
-		if i == cardCount {
-			break
-		}
-
-		newDeck[i] = card
-	}
-
+	copy(newDeck, s[:cardCount])
 	return newDeck
 }
 
-func (s *spaceCardDeck) DrawTopCard() spaceCard {
+func (s *spaceCardDeck) draw() int {
 	topCard := (*s)[0]
 	*s = (*s)[1:]
 
 	return topCard
 }
 
-func (s *spaceCardDeck) DiscardToBottom(card spaceCard) {
-	*s = append(*s, card)
+func (s *spaceCardDeck) discard(card ...int) {
+	*s = append(*s, card...)
 }
 
-func (s spaceCardDeck) Len() int {
-	return len(s)
-}
-
-func (s spaceCardDeck) Score() int {
+func (s spaceCardDeck) score() int {
 	var total int
 	for i, card := range s {
-		total += int(card) * (len(s) - i)
+		total += card * (len(s) - i)
 	}
 
 	return total
 }
 
-func (s spaceCardDeck) String() string {
+func (s *spaceCardDeck) String() string {
 	var sb strings.Builder
-	for i, card := range s {
+
+	// Give the string builder enough room for the string without having to grow.
+	sb.Grow(len(*s) * 4)
+
+	for i, card := range *s {
 		if i > 0 {
 			sb.WriteString(", ")
 		}
 
-		sb.WriteString(card.String())
+		fmt.Fprintf(&sb, "%d", card)
 	}
 
 	return sb.String()
 }
 
-func playGame(gameOutcomes map[string]int, playerDeck1, playerDeck2 *spaceCardDeck, recursive bool) int {
-	initialDeckState := fmt.Sprintf("%s:%s", *playerDeck1, *playerDeck2)
-	if winner, ok := gameOutcomes[initialDeckState]; ok {
-		return winner
-	}
-
+func playGame(playerDeck1, playerDeck2 *spaceCardDeck, recursive bool) int {
 	deckStates := make(map[string]bool)
-	roundNumber := 1
-	for playerDeck1.Len() > 0 && playerDeck2.Len() > 0 {
-		deckState := fmt.Sprintf("%s:%s", *playerDeck1, *playerDeck2)
+	for len(*playerDeck1) > 0 && len(*playerDeck2) > 0 {
+		deckState := playerDeck1.String() + ":" + playerDeck2.String()
 		if val := deckStates[deckState]; val {
-			gameOutcomes[initialDeckState] = 1
 			return 1
 		}
 
 		deckStates[deckState] = true
-
-		playerCard1 := playerDeck1.DrawTopCard()
-
-		playerCard2 := playerDeck2.DrawTopCard()
+		playerCard1 := playerDeck1.draw()
+		playerCard2 := playerDeck2.draw()
 
 		winner := 0
-		if recursive && int(playerCard1) <= playerDeck1.Len() && int(playerCard2) <= playerDeck2.Len() {
-			playerDeckClone1 := playerDeck1.Clone(int(playerCard1))
-			playerDeckClone2 := playerDeck2.Clone(int(playerCard2))
+		if recursive && playerCard1 <= len(*playerDeck1) && playerCard2 <= len(*playerDeck2) {
+			playerDeckClone1 := playerDeck1.clone(playerCard1)
+			playerDeckClone2 := playerDeck2.clone(playerCard2)
 
-			winner = playGame(gameOutcomes, &playerDeckClone1, &playerDeckClone2, recursive)
+			winner = playGame(&playerDeckClone1, &playerDeckClone2, recursive)
 		} else if playerCard1 > playerCard2 {
 			winner = 1
 		} else if playerCard2 > playerCard1 {
@@ -104,24 +82,17 @@ func playGame(gameOutcomes map[string]int, playerDeck1, playerDeck2 *spaceCardDe
 
 		switch winner {
 		case 1:
-			playerDeck1.DiscardToBottom(playerCard1)
-			playerDeck1.DiscardToBottom(playerCard2)
-
+			playerDeck1.discard(playerCard1, playerCard2)
 		case 2:
-			playerDeck2.DiscardToBottom(playerCard2)
-			playerDeck2.DiscardToBottom(playerCard1)
+			playerDeck2.discard(playerCard2, playerCard1)
 		default:
 			panic("Invalid winner specified")
 		}
-
-		roundNumber++
 	}
 
-	if playerDeck1.Len() > 0 {
-		gameOutcomes[initialDeckState] = 1
+	if len(*playerDeck1) > 0 {
 		return 1
-	} else if playerDeck2.Len() > 0 {
-		gameOutcomes[initialDeckState] = 2
+	} else if len(*playerDeck2) > 0 {
 		return 2
 	} else {
 		panic("There should be a winner...")
@@ -133,7 +104,7 @@ func parseInput(lines []string) ([]spaceCardDeck, error) {
 	var current spaceCardDeck
 	for _, line := range lines {
 		if strings.HasPrefix(line, "Player ") {
-			if current.Len() > 0 {
+			if len(current) > 0 {
 				decks = append(decks, current)
 				current = spaceCardDeck{}
 			}
@@ -143,12 +114,11 @@ func parseInput(lines []string) ([]spaceCardDeck, error) {
 				return nil, err
 			}
 
-			card := spaceCard(value)
-			current.DiscardToBottom(card)
+			current.discard(value)
 		}
 	}
 
-	if current.Len() > 0 {
+	if len(current) > 0 {
 		decks = append(decks, current)
 		current = spaceCardDeck{}
 	}
@@ -162,16 +132,16 @@ func run(lines []string, recursive bool) int {
 		log.Fatal(err)
 	}
 
-	gameOutcomes := make(map[string]int)
 	playerDeck1 := spaceCardDecks[0]
 	playerDeck2 := spaceCardDecks[1]
-	playGame(gameOutcomes, &playerDeck1, &playerDeck2, recursive)
+	winner := playGame(&playerDeck1, &playerDeck2, recursive)
 
-	if playerDeck1.Len() > 0 {
-		return playerDeck1.Score()
-	} else if playerDeck2.Len() > 0 {
-		return playerDeck2.Score()
-	} else {
+	switch winner {
+	case 1:
+		return playerDeck1.score()
+	case 2:
+		return playerDeck2.score()
+	default:
 		panic("There should be a winner...")
 	}
 }
