e397635e97312b7d96d37052da90ac718dafcf2d
Kyle Farnung <kfarnung@outlook.com>
2021-04-30 22:08:08 -0700

    Solution for 2019 day 20

---
+++ 2019/src/days/day20.rs
@@ -0,0 +1,22 @@
+pub fn part1(_contents: &str) -> usize {
+    return 0;
+}
+
+pub fn part2(_contents: &str) -> usize {
+    return 0;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(&""), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(&""), 0);
+    }
+}
---
+++ 2019/tests/day20.rs
@@ -0,0 +1,13 @@
+use aoc2019::days::day20::{part1, part2};
+
+#[test]
+fn test_part1() {
+    let content = std::include_str!("../data/day20");
+    assert_eq!(part1(&content), 0);
+}
+
+#[test]
+fn test_part2() {
+    let content = std::include_str!("../data/day20");
+    assert_eq!(part2(&content), 0);
+}
