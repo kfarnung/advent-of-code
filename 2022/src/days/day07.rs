5586c15493762a59ca8836c26377bfaf10fc6fd2
Kyle Farnung <kfarnung@outlook.com>
2023-01-16 14:51:46 -0800

    [2022] Solution for day 7

---
+++ 2022/src/days/day07.rs
@@ -0,0 +1,45 @@
+pub fn part1(_contents: &str) -> usize {
+    0
+}
+
+pub fn part2(_contents: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use std::vec;
+
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        let lines = vec![
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k",
+        ];
+
+        assert_eq!(part1(&lines.join("\n")), 0);
+    }
+}
---
+++ 2022/tests/day07.rs
@@ -0,0 +1,13 @@
+use aoc2022::days::day07::{part1, part2};
+
+#[test]
+fn test_part1() {
+    let content = std::include_str!("../data/day07.txt");
+    assert_eq!(part1(&content), 0);
+}
+
+#[test]
+fn test_part2() {
+    let content = std::include_str!("../data/day07.txt");
+    assert_eq!(part2(&content), 0);
+}
